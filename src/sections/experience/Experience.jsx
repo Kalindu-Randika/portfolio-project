543005a1af4a69c37b4f1b541ffe37f6a4d62663
Kalindu-Randika <kalindu.randika@gmail.com>
2023-12-28 01:17:28 +0530

    Finishing experience section

--- src/sections/experience/Experience.jsx
+++ src/sections/experience/Experience.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import data from "./data";
 import './experience.css';
+import Card from "../../components/Card";
 
 const experience = () => {
     return(
-        <section id='experience'> <h2> experience </h2></section>
+        <section id='experience'> <h2> My Experience </h2>
+        <p> Experienced Product Owner and Software Developer</p>
+            <div className='container services__container'>
+                {
+                    data.map(item=>(
+                        <Card key={item.id} className="service light">
+                            <div className="service__icon"> {item.icon} </div>
+                            <div className="service__details">
+                            <h4> {item.title}</h4>
+                                <p>{item.desc}</p>
+                            </div>
+
+                        </Card>
+                        
+                    ))
+                }
+            </div>
+        </section>
     )
 
 }
