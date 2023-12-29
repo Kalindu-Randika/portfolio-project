83f48766a6d020168aee5c0eccfd1e65ab1c088a
Kalindu-Randika <kalindu.randika@gmail.com>
2023-12-30 02:10:03 +0530

    Working on portfolio

--- src/sections/portfolio/Portfolio.jsx
+++ src/sections/portfolio/Portfolio.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import './portfolio.css';
+import ProjectCategories from "./ProjectCategories";
+import Projects from "./Projects";
+import data from "./data";
+import {useState} from "react";
 
-const portfolio = () => {
+const Portfolio = () => {
+    const [projects, setProjects] = useState(data);
+
+
     return(
-        <section id='portfolio'> <h1> portfolio </h1></section>
+        <section id='portfolio'> <h1> Recent Projects </h1>
+        <p>Check out some of the recent projects I worked on. Use the buttons to toggle the different categories.</p>
+        <div className="container portfolio__container">
+            <ProjectCategories />
+            <Projects projects={projects}/>
+        </div>
+        </section>
     )
 }
 
-export default portfolio;
+export default Portfolio;
---
+++ src/sections/portfolio/Projects.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Project from "./Project";
+
+const Projects = ({projects}) => {
+    return(
+        <div className="portfolio__projects">
+            {
+                projects.map( project => (
+                    <Project />
+                ))
+            }
+
+        </div>
+    )
+}
+
+export default Projects;
